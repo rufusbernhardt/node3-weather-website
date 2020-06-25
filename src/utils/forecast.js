520ed7c0baa07adbb0bd7730efa8d3644b1165f9
Rufus Bernhardt <rufus.bernhardt@stamford.edu>
2020-06-25 10:01:00 +0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (longtitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=75cf759e5fc53089dcfff1a96107cf20&query=' + latitude + ',' + longtitude
+
+    request({ url, json: true }, (error, {body} ) => {
+
+        if(error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' out.')
+        }
+    })
+}
+
+module.exports = forecast
