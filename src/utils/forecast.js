5a3de3c14710d5405f90d8d679b5dc3d9fa269a2
Rufus Bernhardt <rufus.bernhardt@stamford.edu>
2020-06-25 15:51:22 +0700

    Added humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (longtitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' out. The humidity is ' + body.current.humidity + '.')
         }
     })
 }
